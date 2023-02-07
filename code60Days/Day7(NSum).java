21b6f2c41cdc3992221f84a101a6d28c74ce9e5c
Eve He <104244806+Evehe719@users.noreply.github.com>
2023-02-07 04:07:29 -0500

    Create Day7(NSum).java

---
+++ code60Days/Day7(NSum).java
@@ -0,0 +1,101 @@
+454. 4Sum II
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ 
+
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+Output: 2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+Example 2:
+
+Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+Output: 1
+
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int temp;
+        int res = 0;
+        //calculate the sum of two array, and count the times, put it into map
+        for(int i : nums1){
+            for(int j : nums2){
+                temp = i + j;
+                if(map.containsKey(temp)){
+                    map.put(temp, map.get(temp) + 1);
+
+                }else{
+                    map.put(temp, 1);
+                }
+            }
+        }
+        for(int i : nums3){
+            for(int j : nums4){
+                temp = i + j;
+                if(map.containsKey(0 - temp)){
+                    res += map.get(0 - temp);
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+3SUM
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+  
+  
+ public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums.length < 3) return result;
+        Arrays.sort(nums);
+        int i = 0;
+        while(i < nums.length - 2) {
+            if(nums[i] > 0) break;
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                if(sum <= 0) while(nums[j] == nums[++j] && j < k);
+                if(sum >= 0) while(nums[k--] == nums[k] && j < k);
+            }
+            while(nums[i] == nums[++i] && i < nums.length - 2);
+        }
+        return result;
+    }
+}
