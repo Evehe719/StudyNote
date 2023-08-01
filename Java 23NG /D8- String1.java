f3ee08c06e22ebbda4b647549c21ca4f344c25a5
Eve He <104244806+Evehe719@users.noreply.github.com>
2023-08-01 01:39:57 -0700

    Update D8- String1.java

--- Java 23NG /D8- String1.java
+++ Java 23NG /D8- String1.java
@@ -13,11 +13,42 @@ class Solution {
         int i = 0;
         int j = s.length - 1;
         while( i < j){
+           // Swap the characters at the left and right positions.
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
             i++;
             j--;
         }
+    }
+}
+
+541. Reverse String II
+  Example 1:
+
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Example 2:
+
+Input: s = "abcd", k = 2
+Output: "bacd"
+  
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] charArray = s.toCharArray();
+        int n = charArray.length;
+        for(int i = 0; i < n; i += 2 * k){
+            int start = i;
+            int end = Math.min(i + k - 1, n - 1);
+
+            while(start < end){
+                char temp = charArray[start];
+                charArray[start] = charArray[end];
+                charArray[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    return new String(charArray);
     }
 }
